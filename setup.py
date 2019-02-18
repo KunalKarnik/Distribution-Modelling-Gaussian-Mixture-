00f7bd6c6aa0d4795aa12871a0222947a25e484f
Kunal Karnik <kunalkarnik@Kunals-MacBook-Pro.local>
2019-02-18 14:34:33 -0600

    1 Gaussian fit + 2 Mixture2Gaussians fitted to dataset

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='2 data sets with mixture models fitted to features.',
+    author='Kunal A. Karnik & Rohan Choudhari',
+    license='MIT',
+)
